2113bd3f0e8d3f99f01b03764d458834468a4242
marcus <97414481+kiryano@users.noreply.github.com>
2023-11-28 23:00:47 -0800

    started on base class
    started on base class

---
+++ __main__.py
@@ -0,0 +1,44 @@
+import socket
+import ipaddress
+
+class NetworkScanner:
+    """
+    A class to represent a network scanner.
+
+    ...
+
+    Attributes
+    ----------
+    network : str
+        the IP network to scan
+
+    Methods
+    -------
+    scan():
+        Scans the network for active hosts.
+    """
+
+
+def __init__(self, network):
+    """
+    Constructs all the necessary attributes for the network scanner object.
+
+    Parameters
+    ----------
+        network : str
+            the IP network to scan
+    """
+    self.network = network
+
+    pass
+
+def scan(self):
+    """
+    Scans the network for active hosts.
+
+    """
+    pass
+
+if __name__ == "__main__":
+    print("Welcome to the Network Scanner!")
+    
